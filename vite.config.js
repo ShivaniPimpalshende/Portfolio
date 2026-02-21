19b482ddcdce53fd387fc53adb47d6fb239fce4d
Shivani Pimpalshende <159690966+ShivaniPimpalshende@users.noreply.github.com>
2026-02-21 15:06:36 +0530

    Update Vite config to include preview settings
    Added preview configuration with host, port, and allowed hosts.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//   ],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  preview: {
+    host: true,
+    port: process.env.PORT,
+    allowedHosts: ["portfolio-zau9.onrender.com"]
+  }
 })
